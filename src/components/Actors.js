97b71619d6d86576516a0007dc1322bdd878aa59
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-07-05 17:04:42 +0930

    implement Actors component

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,8 +3,19 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
-      
+    <div style={{lineHeight: "200%"}}>
+      <h1>Actors Page</h1>
+      {actors.map(actor => 
+        <div>
+          <h2>Name: {actor.name}</h2>
+          <div>
+            Movies:
+            <ul>
+              {actor.movies.map(movie => <li>{movie}</li>)}
+            </ul>
+          </div>
+        </div>
+        )}
     </div>
   );
 };
