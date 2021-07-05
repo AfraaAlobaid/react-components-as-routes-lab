4ca71993178184905d35ee8902fc487899054a05
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-07-05 16:58:36 +0930

    implement Movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,19 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div style={{lineHeight: "200%"}}>
+        <h1>Movies Page</h1>
+        {movies.map(movie => <div>
+          <h2>Name: {movie.title}</h2>
+          <div> Time: {movie.time} </div>
+          <div> Genre: 
+          <ul>
+            {movie.genres.map(item => <li>{item}</li>)}
+          </ul>
+          </div>
+
+        </div>)}
+
     </div>
   );
 };
